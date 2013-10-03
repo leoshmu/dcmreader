31c60249e728c018cf7c0253e38d38496e34fac6
Leo Shmuylovich <leoshmu@gmail.com>
2013-10-03 09:42:17 -0500

    add more helpful constants, particulary those related to data elements with undefined length

--- app/scripts/services/DicomConstants.js
+++ app/scripts/services/DicomConstants.js
@@ -5,6 +5,9 @@ angular.module('dcmreaderApp')
 
     // Public API here
     return {
+      "HEADER_ZERO_OFFSET":128,
+      "POST_HEADER_TAG_OFFSET":132,
+      "DICM_START_TAG":'4944,4D43',
       "TRANSFER_SYNTAX_UID_TAG": '0002,0010',
       "RAW_EXPLICIT_BIG": '1.2.840.10008.1.2.2',
       "RAW_EXPLICIT_LITTLE": '1.2.840.10008.1.2.1',
@@ -12,6 +15,15 @@ angular.module('dcmreaderApp')
       "PIXEL_DATA_TAG": '7FE0,0010',
       "IMAGE_WIDTH_TAG": '0028,0011',
       "IMAGE_HEIGHT_TAG": '0028,0010',
-      "PLANAR_CONFIGURATION_TAG": '0028,0006'
+      "PLANAR_CONFIGURATION_TAG": '0028,0006',
+      "PHOTOMETRIC_INTERPRETATION_TAG": '0028,0004',
+      'IMPLICIT_TAGS':['FFFE,E000','FFFE,E00D', 'FFFE,E0DD'],
+      'ITEM':'FFFE,E000',
+      'ITEM_DELIMITATION_ITEM':'FFFE,E00D',
+      'SEQUENCE_DELIMITATION_ITEM':'FFFE,E0DD',
+      'UNDEFINED_LENGTH':'FFFF,FFFF',
+      'SEQUENCE_VR':'SQ',
+      "VR_32BIT_CODES":['OB', 'OW', 'OF', 'SQ', 'UT', 'UN']
+
     };
   });

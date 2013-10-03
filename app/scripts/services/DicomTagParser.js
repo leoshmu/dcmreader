26b30b158eebc60a27df2f1acfa2bea53c0a674b
Leo Shmuylovich <leoshmu@gmail.com>
2013-10-03 10:08:03 -0500

    Major refactor, now supports parsing data elements with unknown length, sequences of data elements with embedded data elements, and no need for a tag to be in the tag dictionary in order to load the full tag data. Should support a much wider array of DICM files

--- app/scripts/services/DicomTagParser.js
+++ app/scripts/services/DicomTagParser.js
@@ -4,12 +4,21 @@ angular.module('dcmreaderApp')
   .factory('DicomTagParser', function (DicomPixelReader, DicomConstants, TagDictionary, ValueRepresentationDictionary) {
     // Service logic
     // extract the name of the tag
-    var is_tag_type2 = function(tag_string){
-      return (tag_string.substring(0,3) === '0002');
+    var is_tag_group2 = function(tag_string, is_little_endian){
+      var group_string = tag_string.substring(0,4);
+      if(tag_string=="0200,1200"){
+        // debugger
+      }
+      if(is_little_endian){
+        return (group_string === '0002');
+      } else {
+        return (group_string === '0200');
+      }
     }
     var read_tag_string = function(dv, offset, is_little_endian){
         var byte0 = dv.getUint16(offset, is_little_endian);
         var byte1 = dv.getUint16(offset+2, is_little_endian);
+
         var tag_string = ((0xffff+1)| byte0).toString(16).slice(1).toUpperCase() +','+ ((0xffff+1)| byte1).toString(16).slice(1).toUpperCase();
         return tag_string;
     }
@@ -36,102 +45,220 @@ angular.module('dcmreaderApp')
         info: vr_info
       }
     }
-    // read the Value Length
-    var read_vl = function(dv, tag_start_offset, is_little_endian, vr){
-      var vl, vl_offset, value_offset;
-      if(!vr){
-        // implicit will have no vr defined
-        vl_offset = tag_start_offset + 4;
-        value_offset = vl_offset + 4;
-        vl = dv.getUint32(vl_offset, is_little_endian);
-      } else {
-        if(_.contains(['OB', 'OW', 'OF', 'SQ', 'UT', 'UN'], vr)){
-          vl_offset = tag_start_offset + 8;
-          value_offset = vl_offset + 4;
-          vl = dv.getUint32(vl_offset, is_little_endian);
-        } else {
-          vl_offset = tag_start_offset + 6;
-          value_offset = vl_offset + 2;
-          vl = dv.getUint16(vl_offset, is_little_endian);
-        }
+    var get_implicit_vr = function(tag){
+      var vr = TagDictionary.getTagVR(tag);
+      var vr_info = ValueRepresentationDictionary.lookup(vr);
+      return {
+        code: vr,
+        info: vr_info
       }
-      return {length: vl, offset: value_offset}
     }
-    var read_non_pixel_data = function(dv, tag, is_little_endian){
+
+    var read_non_pixel_data = function(dv, value_offset, value_length, vr_type, FILE_SETTINGS){
+      var is_little_endian = FILE_SETTINGS.is_little_endian;
       var tag_value;
-      switch(tag.vr.info.type){
+      switch(vr_type){
             case 'char string':
-              tag_value = concatenateBytes(dv, tag.value_offset, tag.length, {is_little_endian:is_little_endian, convert_to_char: true});
+              tag_value = concatenateBytes(dv, value_offset, value_length, {is_little_endian:is_little_endian, convert_to_char: true});
             break;
             case 'Uint16':
-              tag_value = dv.getUint16(tag.value_offset, is_little_endian);
+              tag_value = dv.getUint16(value_offset, is_little_endian);
             break;
             case 'Uint32':
-              tag_value = dv.getUint32(tag.value_offset, is_little_endian);
+              tag_value = dv.getUint32(value_offset, is_little_endian);
             break;
             case 'byte string':
-              tag_value = concatenateBytes(dv, tag.value_offset, tag.length, {is_little_endian:is_little_endian});
+              tag_value = concatenateBytes(dv, value_offset, value_length, {is_little_endian:is_little_endian});
+            break;
+            case 'signed int32':
+              tag_value = dv.getInt32(value_offset, is_little_endian);
+            break;
+            case 'signed int16':
+              tag_value = dv.getInt16(value_offset, is_little_endian);
+            break;
+            case 'Float64':
+              tag_value = dv.getFloat64(value_offset, is_little_endian);
+            break;
+            case 'Float32':
+              tag_value = dv.getFloat32(value_offset, is_little_endian);
+            break;
+            case 'Float16':
+              tag_value = dv.getFloat16(value_offset, is_little_endian);
+            break;
+            default:
+              // console.log(tag)
             break;
           }
       return tag_value;
     }
+    var is_tag_implicit = function(tag){
+      return _.contains(DicomConstants.IMPLICIT_TAGS, tag);
+    };
+    var parse_undefined_length_item_value = function(dv, offset, FILE_SETTINGS){
+        // ends with the ITEM DELIMITER TAG
+      var is_item_end_found = false;
+      var item_value = [];
+      while(!is_item_end_found){
+        var data_element = parse_data_element(dv, offset, FILE_SETTINGS);
+        item_value.push(data_element);
+        offset = data_element.next_offset;
+
+        if (data_element.tag == DicomConstants.ITEM_DELIMITATION_ITEM){
+
+          is_item_end_found = true;
+        }
+      }
+      return {value: item_value, next_offset: offset};
+    };
+
+    var parse_data_element = function(dv, data_element_offset, FILE_SETTINGS){
+      // ALL Data Elements have the following structure
+      // 4 byte tag string (2 bytes Group Number + 2 bytes Element Number)
+      // 0, 2, or 4 byte VR (2 bytes + 2reserved bytes 0000H). 0 is if the data is implicit. 2 vs 4 depends on type of VR
+      // 2 or 4 bytes for Value Length depending on VR length ( VR 2 VL 2, VR 0||4 VL 4)
+      // VL length or undefined length
+      //
+      //
+      var
+        tag,
+        vr,
+        vr_type,
+        value_length,
+        vl_tag,
+        value_length_bytes,
+
+        value_length_offset,
+        value_field,
+        value_field_offset,
+        next_offset;
+      var is_little_endian = FILE_SETTINGS.is_little_endian;
+      var is_implicit = FILE_SETTINGS.is_implicit;
+      // first read the tag of the data element
+      tag = read_tag_string(dv, data_element_offset, is_little_endian);
+      // if the tag is group 2, then reread the tag with little endian
+      if(!is_little_endian && is_tag_group2(tag, is_little_endian)){
+        // group 2 is little endian explicit
+        is_little_endian = true;
+        tag = read_tag_string(dv, data_element_offset, is_little_endian);
+      }
+      // next, read the vr, unless if we have implicit encoding, or if the tag implies implicit encoding
+
+      value_length_bytes = 32;
+      if(is_implicit || is_tag_implicit(tag)){
+        var vr_object = get_implicit_vr(tag);
+        vr_type = (vr_object.info) ? vr_object.info.type : false;
+        value_length_offset = data_element_offset + 4; // tag is 4 bytes, and vr is not present
+      } else {
+        var vr_object = read_vr(dv, data_element_offset + 4, is_little_endian);
+        vr = vr_object.code;
+        vr_type = vr_object.info.type;
+        // value length is 32 bytes unless specific explicit non 32 bit vr!
+        if(!_.contains(DicomConstants.VR_32BIT_CODES,vr)){
+          value_length_bytes = 16;
+          value_length_offset = data_element_offset + 6; // tag is 4 bytes, and vr is 2 bytes
+        } else {
+          value_length_offset = data_element_offset + 8; // tag is 4 bytes, and vr is 4 bytes (2 bytes of vr plus 2 bytes reserved)
+        }
+      }
+      // next, read the value length
+      // value llength is 16 bit if vr is explicit and not OB, OW, OF, SQ, UT, UN
+      if(value_length_bytes==16){
+        value_length = dv.getUint16(value_length_offset, is_little_endian);
+        value_field_offset = value_length_offset + 2;
+      } else {
+        value_length = dv.getUint32(value_length_offset, is_little_endian);
+        value_field_offset = value_length_offset + 4;
+        vl_tag = read_tag_string(dv, value_length_offset, is_little_endian);
+        if(vl_tag == DicomConstants.UNDEFINED_LENGTH){
+          value_length = undefined;
+        }
+      }
+      // finally, read the value field
+      // if we have a vr of SQ, then we need special code to unpack
+      // data elements inside the VALUE of a SEQUENCE
+      if(vr == DicomConstants.SEQUENCE_VR){
+        // read a sequence
+        var info = read_sequence_data_elements(dv, value_field_offset, value_length, FILE_SETTINGS)
+        value_field = info.value;
+        next_offset = info.next_offset;
+      } else {
+
+        // if the value length is undefined, then we need to read an undefined value
+        if(value_length==undefined){
+          var parsed_item = parse_undefined_length_item_value(dv, value_field_offset, FILE_SETTINGS);
+          value_field = parsed_item.value;
+          next_offset = parsed_item.next_offset;
+        } else {
+          if(tag == DicomConstants.PIXEL_DATA_TAG){
+            //read pixel data
+            console.log('pixel data time', tag, vr_object, FILE_SETTINGS)
+            value_field = DicomPixelReader.read(dv, value_field_offset, value_length, FILE_SETTINGS);
+          } else {
+            value_field = read_non_pixel_data(dv, value_field_offset, value_length, vr_type, FILE_SETTINGS);
+          }
+          next_offset = value_field_offset + value_length;
+        }
+      }
+      return {
+        tag: tag,
+        vr: vr,
+        vr_object: vr_object,
+        value_length: value_length,
+        tag_description : TagDictionary.getTagDescription(tag),
+        value_length_offset : value_length_offset,
+        value_field_offset: value_field_offset,
+        value_field:value_field,
+        offset: data_element_offset,
+        next_offset : next_offset
+      }
+    };
+
+    var read_sequence_data_elements = function(dv, value_offset, value_length, FILE_SETTINGS){
+      var offset = value_offset;
+      var data_element_value_array = [];
+      var parsed_data_element;
+      if(value_length){
+        // parse sequence of known length
+      } else {
+        var is_sequence_end_found = false;
+        var tag_string;
+        var next_offset;
+        while(!is_sequence_end_found){
+          parsed_data_element = parse_data_element(dv, offset, FILE_SETTINGS);
+          data_element_value_array.push(parsed_data_element);
+          offset = parsed_data_element.next_offset;
+
+          if(parsed_data_element.tag == DicomConstants.SEQUENCE_DELIMITATION_ITEM){
+            is_sequence_end_found = true;
+          }
+        }
+      }
+      return {value: data_element_value_array, next_offset: offset}
+    };
+
 
     // Public API here
     return {
-      parse_tag: function(dv, offset, is_little_endian){
-        // if we can't read 2 bytes, then get out
-        if (dv.byteLength<=offset+2){
-
-          // TODO
-          // throw and exception
-          throw new Error('trying to read too far into data view stream');
+      check_start_tag: function(dv){
+        // the dicm standard dictates that the start shall be
+        // 128 bytes of 0, followed by letters DICM
+        var start_offset = DicomConstants.HEADER_ZERO_OFFSET;
+        var is_little_endian = true;
+        var tag_string = read_tag_string(dv,start_offset, is_little_endian);
+        if (tag_string == DicomConstants.DICM_START_TAG){
+          return true;
+        } else {
           return false;
-        }
-        var tag_string = read_tag_string(dv, offset, is_little_endian);
-        // if the tag string is not found in the dictionary, then exit
-        if(!TagDictionary.getTagDescription(tag_string)){
-          // TODO
-          // throw an exception
-          return false;
-        }
-        if(is_tag_type2(tag_string)){
-          // group 2 is little endian explicit
-          is_little_endian = true;
-        }
-        var vr_offset = offset + 4; // 4 bytes for the tag name
-        var vr = read_vr(dv, vr_offset, is_little_endian);
-        var value_info = read_vl(dv, offset, is_little_endian, vr.code);
-        var next_offset = value_info.offset + value_info.length;
-        return {
-          tag: tag_string,
-          data: {
-            offset: offset,
-            value_offset: value_info.offset,
-            tag_description: TagDictionary.getTagDescription(tag_string),
-            vr: vr,
-            length: value_info.length,
-            // value: tag_value,
-          },
-          next_offset: next_offset
         }
       },
-      read_tag_value: function(dv, tag, dicom_hash, is_little_endian){
-        var tag_value;
-        // if the tag is pixel data, then treat it differently
-        if (tag.tag == DicomConstants.PLANAR_CONFIGURATION_TAG){
-          console.log(tag, is_little_endian, read_non_pixel_data(dv, tag.data, is_little_endian));
-        }
-        if (tag.tag == DicomConstants.PIXEL_DATA_TAG){
-          console.log(dicom_hash[DicomConstants.PLANAR_CONFIGURATION_TAG])
-          var options = {
-            is_little_endian: is_little_endian,
-            is_planar_configuration: dicom_hash[DicomConstants.PLANAR_CONFIGURATION_TAG].value
-          }
-          tag_value = DicomPixelReader.read(dv, tag.data, options);
-        } else {
-          tag_value = read_non_pixel_data(dv, tag.data, is_little_endian);
-        }
-        return tag_value;
+
+      parse_data_element: function(dv, offset, FILE_SETTINGS){
+
+        return parse_data_element(dv, offset, FILE_SETTINGS);
       }
+
+      // read the value of a tag at a certain position
+      // and return the offset after the value
+
     };
   });

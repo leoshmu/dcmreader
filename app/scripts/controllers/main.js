75dc319a2fde104794747fe476bc3854e39295eb
Leo Shmuylovich <leoshmu@gmail.com>
2013-10-03 09:41:17 -0500

    add FileManager service to main controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('dcmreaderApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', ['$scope', 'FileManager', function ($scope,FileManager) {
+
+    $scope.removeFile = function(id){
+      FileManager.remove(id);
+    }
+    $scope.fileManager = FileManager;
+
     $scope.tag_hash = {};
     $scope.tag_array = [];
     $scope.img_data = {};
